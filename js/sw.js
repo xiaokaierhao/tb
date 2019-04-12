fee6bda8e36b31a5c72ce69b95582b07cd090c81
xiaokaierhao <lkrexe@qq.com>
2019-04-12 19:22:37 +0800

    页脚尚未完善

--- js/sw.js
+++ js/sw.js
@@ -9,4 +9,10 @@ var mySwiper = new Swiper ('.swiper-container', {
     },
 
 
-  })
+  });
+  var mySwiper = new Swiper ('.ttswcon', {
+    direction: 'vertical', // 垂直切换选项
+    loop: true, // 循环模式选项
+    autoplay:true,
+  }
+  )
